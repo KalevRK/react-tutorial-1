fd73424245d1fbcdb0c9125becbbd71ad9046485
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-07-05 21:28:38 -0700

    Remove hardcoded comments and refactor to use data passed into CommentBox component.

--- tutorial.js
+++ tutorial.js
@@ -1,22 +1,36 @@
+// data for Comments
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 var CommentBox = React.createClass({
   render: function() {
   	return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data}/>
         <CommentForm />
       </div>
   	);
   }
 });
 
-React.render(<ComentBox />, document.getElementById('content'));
+React.render(<CommentBox data={data} />, document.getElementById('content'));
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        {commentNodes}    
       </div>
     );
   }
@@ -27,6 +41,20 @@ var CommentForm = React.createClass({
     return (
       <div className="commentForm">
         Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </div>
     );
   }
